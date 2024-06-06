856d4ced629864930e5d7bb519e4f85604720e68
rafaelagonzaga <105285228+rafaelagonzaga@users.noreply.github.com>
2024-06-06 10:29:46 -0300

    pjokopijm

---
+++ script.js
@@ -0,0 +1,12 @@
+const caixaPrincipal = document.querySelector ("caixa-principal");
+const caixaPerguntas = document.querySelector("caixa-pergunda");
+const caixaAlternativa = document.querySelector("caixa-alternativa");
+const caixaResultado = document.querySelector("caixa-resultado");
+const textoResultado = document.querySelector("texto-resultado");
+
+const perguntas = [     
+    { // primeiro objeto da lista de perguntas}
+
+
+
+]
