87ac3e3f649e9a2faf18c4faf09ced19e7b706a9
rafaelagonzaga <105285228+rafaelagonzaga@users.noreply.github.com>
2024-06-06 10:36:09 -0300

    rthxf

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const caixaResultado = document.querySelector("caixa-resultado");
 const textoResultado = document.querySelector("texto-resultado");
 
 const perguntas = [     
-    { // primeiro objeto da lista de perguntas}
-
+    { // primeiro objeto da lista de perguntas  
+         enunciado: "pergunta1",
+         alternativas: ["alternativa1" , "alternativa2" ]
+         },
+         { // segundo objeto da lista de perguntas  
+            enunciado: "pergunta2",
+            alternativas: ["alternativa1" , "alternativa2" ]
+            }
 
 
 ]
